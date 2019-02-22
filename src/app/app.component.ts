2f7f6594bcce41d45c71d9a45082393cc06f8e13
Anna <aaraszewska@gmail.com>
2019-02-22 10:16:14 +0000

    modulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularTest';
-}
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+  }
+  remove(tasks: string) {
+    this.tasksList = this.tasksList.filter( e => e !== tasks);
+  }
+
+  done(tasks: string) {
+    this.tasksDone.push(tasks);
+    this.remove(tasks);
+ }
+  }
+
+ 
+
+
+  
+
+
+  
+
+  
